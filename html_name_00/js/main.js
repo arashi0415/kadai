73ffcd52703e28a54d26da4671dcf98c8428dce1
arashi0415 <arachi0415@gmail.com>
2024-02-13 12:39:46 +0900

    test

--- html_name_00/js/main.js
+++ html_name_00/js/main.js
@@ -1,1 +1,30 @@
-// jsを記述する際はここに記載していく
+"use strict"
+
+const arays = [1,2,3,4];
+
+function result (aray){
+    const arays = [1,2,3,4];
+    return aray.map(x => x+2);
+}
+console.log(result(arays));
+
+
+const objectArray = profile();
+
+
+
+function profile() {
+    const obj = {
+        name: [Text = "たこ", "イカ"],
+        age: [String = 28],
+        likes: [Text = "ギャン"]
+    };
+
+    const objectArray = Object.keys(obj).map(key => {
+        const value = obj[key];
+        value["id"] = key;
+        return value;
+    });
+    return objectArray;
+}
+console.log(objectArray);
